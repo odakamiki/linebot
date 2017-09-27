80137b1ef16b786909b83177b6319a75018ead30
unknown <user@odaka-ch.local>
2017-09-27 09:45:19 +0900

    学校からのコミット

--- app.js
+++ app.js
@@ -13,10 +13,13 @@ function getMessageText(text) {
   if(text.indexOf('漢字') >= 0 ){
     message ='小高産業技術高校では毎週水曜日の国語の時間に漢字のテストがあります。'
 
-  }else  if(text.indexOf('敬え') >= 0 ){
+  }else if(text.indexOf('敬え') >= 0 ){
       message ='かしこまりました。'
 
-    }
+    }else if(text.indexOf('スマホゲー') >= 0 ){
+        message ='金がものをいう'
+
+      }
 
   return message;
 }

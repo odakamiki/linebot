708e26c4391bda5365530f79763ed218722ad6fe
unknown <user@odaka-ch.local>
2017-09-29 09:36:28 +0900

    学校からのコミット

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function getMessageText(text) {
         message ='金がものをいう'
 
       }else if(text.indexOf('今の時間'||'今何時') >= 0 ){
-      message =hour+'時'+minute+'分'
+      message =(var)hour+'時'+(var)minute+'分'
 
        }else if(text.indexOf('好きな'&&'ドラクエ') >= 0 ){
        message ='7の暗さと10の賑やかさが特に好きです。'
